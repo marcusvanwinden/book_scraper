e7272f037f7aee48bb0257910f8175a7a84338a6
Marcus van Winden <marcus.van.winden@icloud.com>
2020-04-10 20:38:29 -0700

    Implement MSSQL and write comments in code

--- app.py
+++ app.py
@@ -1,41 +1,71 @@
-import mysql.connector
+# Import required packages
+import pyodbc
 import operations
 import os
 import time
 
-
-database = mysql.connector.connect(
-    host = "localhost",
-    user = os.environ["MYSQL_USER"],
-    password = os.environ["MYSQL_PASS"],
-    database = ""
+# Establish connection with a MSSQL database
+connection = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    f"SERVER={os.environ.get('DB_SERVER')};"
+    f"DATABASE={os.environ.get('DB_DATABASE')};"
+    f"UID={os.environ.get('DB_USER')};"
+    f"PWD={os.environ.get('DB_PASSWORD')};",
+    autocommit=True
 )
 
-cursor = database.cursor()
+# Instantiate cursor
+cursor = connection.cursor()
 
-cursor.execute("DROP DATABASE IF EXISTS bookcase")
-cursor.execute("CREATE DATABASE bookcase")
-cursor.execute("USE bookcase")
+# Drop table Books if it already exists
 cursor.execute("""
-    CREATE TABLE books (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        title VARCHAR(100) NOT NULL,
-        price DECIMAL(5, 2),
-        stock INT
+    IF OBJECT_ID('[dbo].[Books]') IS NOT NULL
+        DROP TABLE [dbo].[Books]
+""")
+
+# Create the books table
+cursor.execute("""
+    CREATE TABLE [dbo].[Books] (
+        [BookId] INTEGER IDENTITY(1, 1),
+        [Title] VARCHAR(100) NOT NULL,
+        [Price] DECIMAL(5, 2),
+        [Stock] INTEGER,
+        CONSTRAINT PK_BookId PRIMARY KEY([BookId])
     )
 """)
 
 if __name__ == "__main__":
-    print("Hello! \U0001f600")
-    time.sleep(1)
-    book_urls = operations.scrape_book_urls(begin=1)
+    # Print welcome screen
+    operations.clear_screen()
+    print("Hello \U0001f600\n")
+    time.sleep(2)
+    begin_page = None
+    # Ask user for valid page number
+    while begin_page not in range(1, 51):
+        try:
+            print("At what page would you like to start scraping?\n\n"
+                  "Please type a number between 1 and 50.\n\n"
+                  "The higher the number, the faster the program will be done.\n"
+            )
+            begin_page = int(input("Number > "))
+            if begin_page not in range(1, 51):
+                raise Exception()
+        except:
+            operations.clear_screen()
+    # Start scraping
+    operations.clear_screen()
+    print("Let the scraping begin! \U0001f600")
+    time.sleep(2)
+    book_urls = operations.scrape_book_urls(begin=begin_page)
     books = operations.scrape_books(book_urls)
     operations.write_to_csv_file(books)
 
     try:
         current_book = 1
         for book in books:
-            query = "INSERT INTO books (title, price, stock) VALUES (%s, %s, %s)"
+            query = """
+                INSERT INTO [dbo].[Books] (title, price, stock)
+                VALUES (?, ?, ?)"""
             cursor.execute(query, (book["title"], book["price"], book["stock"]))
             print(f"Saving book {current_book}/{len(books)} to the database")
             current_book += 1
@@ -43,5 +73,7 @@ if __name__ == "__main__":
     except:
         pass
 
-    database.commit()
+    # Commit data to the database
+    connection.commit()
+    operations.clear_screen()
     print("Completed!\nNow type 'open books.csv' \U0001f600")
--- operations.py
+++ operations.py
@@ -1,9 +1,10 @@
-from bs4 import BeautifulSoup
-from csv import DictWriter, DictReader
-from requests import get
-from re import search
-from time import sleep
-
+# Import required packages
+import bs4
+import csv
+import requests
+import re
+import time
+import os
 
 BASE_URL = "http://books.toscrape.com/"
 
@@ -13,8 +14,8 @@ def scrape_book_urls(begin=1):
     page_url = f"catalogue/page-{begin}.html"
     while page_url:
         print(f"Scraping {BASE_URL}{page_url}")
-        response = get(f"{BASE_URL}{page_url}")
-        soup = BeautifulSoup(response.text, "html.parser")
+        response = requests.get(f"{BASE_URL}{page_url}")
+        soup = bs4.BeautifulSoup(response.text, "html.parser")
         books = soup.find_all(class_="product_pod")
         book_urls.extend(f"catalogue/{book.h3.a['href']}" for book in books)
         next_button = soup.find(class_="next")
@@ -26,11 +27,11 @@ def scrape_books(book_urls):
     books = []
     current_book = 1
     for book_url in book_urls:
-        response = get(f"{BASE_URL}{book_url}")
-        soup = BeautifulSoup(response.text, "html.parser")
+        response = requests.get(f"{BASE_URL}{book_url}")
+        soup = bs4.BeautifulSoup(response.text, "html.parser")
         title = soup.find("h1").get_text(strip=True)
         price = float(soup.find(class_="price_color").get_text(strip=True)[2:])
-        stock = int(search("\d+", soup.find(class_="instock availability").get_text(strip=True)).group())
+        stock = int(re.search("\d+", soup.find(class_="instock availability").get_text(strip=True)).group())
         books.append({"title": title, "price": price, "stock": stock})
         print(f"Scraping book {current_book}/{len(book_urls)}")
         current_book += 1
@@ -41,18 +42,22 @@ def write_to_csv_file(books):
     current_book = 1
     with open("books.csv", "w") as csv_file:
         fieldnames = ["title", "price", "stock"]
-        dict_writer = DictWriter(csv_file, fieldnames=fieldnames)
+        dict_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         dict_writer.writeheader()
         for book in books:
             print(f"Writing book {current_book}/{len(books)} to the csv file")
             dict_writer.writerow(book)
             current_book += 1
-            sleep(0.1)
+            time.sleep(0.1)
 
 
 def get_all_books():
     books = []
     with open("books.csv", "r") as csv_file:
-        dict_reader = DictReader(csv_file)
+        dict_reader = csv.DictReader(csv_file)
         books.extend(book for book in dict_reader)
     return books
+
+
+def clear_screen():
+    os.system("cls") if os.name == "nt" else os.system("clear")
